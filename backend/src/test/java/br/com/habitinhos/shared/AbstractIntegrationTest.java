75607c88b66df20f155e8d6904ecb39a1cd61f43
dmagliano <diogo.magliano@gmail.com>
2026-05-30 23:54:53 -0300

    Handle missing Docker for integration tests

--- backend/src/test/java/br/com/habitinhos/shared/AbstractIntegrationTest.java
+++ backend/src/test/java/br/com/habitinhos/shared/AbstractIntegrationTest.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers(disabledWithoutDocker = true)
 public abstract class AbstractIntegrationTest {
 
   @ServiceConnection
